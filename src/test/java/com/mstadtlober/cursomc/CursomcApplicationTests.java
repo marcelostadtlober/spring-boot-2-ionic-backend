bde8ebd2f59929843e03798be080252aa885fc44
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-02-20 17:01:06 -0300

    Projeto criado

---
+++ src/test/java/com/mstadtlober/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstadtlober.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
