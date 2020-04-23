80c486f75bbdff5b66f114f5c08faa487cb65db5
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-23 14:40:39 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/mstadtlober/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/mstadtlober/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.mstadtlober.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.mstadtlober.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;	
+	private Integer id;
+	
+	@NotEmpty(message = "Preencimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/mstadtlober/cursomc/services/CategoriaService.java
+++ src/main/java/com/mstadtlober/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mstadtlober.cursomc.domain.Categoria;
+import com.mstadtlober.cursomc.dto.CategoriaDTO;
 import com.mstadtlober.cursomc.repositories.CategoriaRepository;
 import com.mstadtlober.cursomc.services.exceptions.DataIntegrityException;
 import com.mstadtlober.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
