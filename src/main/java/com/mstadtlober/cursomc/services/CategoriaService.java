0295232c54f30db656ea39e1c446fc9b369f2291
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-22 23:52:58 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/mstadtlober/cursomc/services/CategoriaService.java
+++ src/main/java/com/mstadtlober/cursomc/services/CategoriaService.java
@@ -11,14 +11,20 @@ import com.mstadtlober.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		
+		return repo.save(obj);
 	}
 
 }
