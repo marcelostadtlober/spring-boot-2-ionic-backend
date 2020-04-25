6d4a7ce21997f34d429349766a1a737fa5cf3e8f
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-25 18:01:30 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/mstadtlober/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mstadtlober/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.mstadtlober.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mstadtlober.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
