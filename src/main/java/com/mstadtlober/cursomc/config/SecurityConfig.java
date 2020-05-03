f9a68c3b2bbdf1f1ecbe9792115814dbcaecd10e
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-05-02 23:54:45 -0300

    Implementando autorização

--- src/main/java/com/mstadtlober/cursomc/config/SecurityConfig.java
+++ src/main/java/com/mstadtlober/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mstadtlober.cursomc.security.JWTAuthenticationFilter;
+import com.mstadtlober.cursomc.security.JWTAuthorizationFilter;
 import com.mstadtlober.cursomc.security.JWTUtil;
 
 @Configuration
@@ -60,6 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
