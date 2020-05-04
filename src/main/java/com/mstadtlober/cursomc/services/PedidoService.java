301c47399938fc550e87c52c2d744f3340e94dbd
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-05-04 18:17:53 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/mstadtlober/cursomc/services/PedidoService.java
+++ src/main/java/com/mstadtlober/cursomc/services/PedidoService.java
@@ -6,9 +6,13 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mstadtlober.cursomc.domain.Cliente;
 import com.mstadtlober.cursomc.domain.ItemPedido;
 import com.mstadtlober.cursomc.domain.PagamentoComBoleto;
 import com.mstadtlober.cursomc.domain.Pedido;
@@ -16,6 +20,8 @@ import com.mstadtlober.cursomc.domain.enums.EstadoPagamento;
 import com.mstadtlober.cursomc.repositories.ItemPedidoRepository;
 import com.mstadtlober.cursomc.repositories.PagamentoRepository;
 import com.mstadtlober.cursomc.repositories.PedidoRepository;
+import com.mstadtlober.cursomc.security.UserSS;
+import com.mstadtlober.cursomc.services.exceptions.AuthorizationException;
 import com.mstadtlober.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -76,6 +82,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
